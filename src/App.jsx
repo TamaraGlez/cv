210b406af274a73753d8a38681f8f02d1d0d7d2d
TamaraGlez <tamara.gonzalez@bootcamp-upgrade.com>
2023-01-25 12:56:58 +0100

    feat about

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import './App.scss';
-import About from './components/About';
-import Education from './components/Education';
-import Hero from './components/Hero';
-import { CV } from './CV/CV';
+import { useState } from "react";
+import "./App.scss";
+import About from "./components/About";
+import Education from "./components/Education";
+import Hero from "./components/Hero";
+import { CV } from "./CV/CV";
 
 function App() {
-  console.log(CV)
   //destruccturing de cv
-  const {hero, education, experience, habilities, unregulatedStudies } = CV
-  
+  const { hero, education, experience, habilities, unregulatedStudies } = CV;
+  const [mostrar, setMostrar] = useState('About')
+  console.log(mostrar)
   return (
-
-    <div className="App">
-      <Hero hero={hero}/>
-      <About about={hero.aboutMe} />
-      <Education education={education} ></Education>
-
+    <div>
+      <div className="App">
+        <Hero hero={hero} />
+        <button type="button" onClick={() => setMostrar("About")}>sobre mi</button>
+        <About about={hero.aboutMe} />
+        <Education education={education}></Education>
+      </div>
     </div>
   );
 }
