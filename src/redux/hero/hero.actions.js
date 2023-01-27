a632dc5b8593c46f38435036714a9978ebd11ff6
TamaraGlez <tamara.gonzalez@bootcamp-upgrade.com>
2023-01-27 12:56:05 +0100

    copiamos redux

---
+++ src/redux/hero/hero.actions.js
@@ -0,0 +1,9 @@
+import store from "../store";
+const { dispatch } = store;
+
+export const setHero = (data) => {
+    dispatch({
+        type: 'SET_HERO',
+        payload: data,
+    })
+}
---
+++ src/redux/hero/hero.reducer.js
@@ -0,0 +1,13 @@
+const INITAL_STATE = {
+    hero: {},
+};
+
+export const heroReducer = (state = INITAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_HERO': {
+            return { ...state, hero: action.payload }
+        }
+        default:
+            return state;
+    }
+};
